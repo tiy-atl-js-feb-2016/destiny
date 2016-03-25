977a3d9a2f36fcbf08446cb5b46039561ae0bff6
JD Isaacks <jd@jisaacks.com>
2016-03-25 11:25:17 -0400

    Convert to classes

--- src/js/index.js
+++ src/js/index.js
@@ -3,31 +3,47 @@
 import $ from 'jquery';
 import scenarios from './scenarios';
 
-function slide(scenario) {
-  var buttons = scenario.paths.map(path => {
-    return `<li><button data-scenario="${path.scenario}">${path.label}</button></li>`;
-  })
+class Game {
+  constructor(scenarios) {
+    this.scenarios = scenarios;
+    this.paths = [];
+  }
 
-  return `
-    <h4>${scenario.message}</h4>
-    <ul>${buttons.join('')}<ul>
-  `
+  slide(scenario) {
+    var buttons = scenario.paths.map(path => {
+      return `<li><button data-scenario="${path.scenario}">${path.label}</button></li>`;
+    })
+
+    return `
+      <h4>${scenario.message}</h4>
+      <ul>${buttons.join('')}<ul>
+    `
+  }
+
+  pathsLengthSum() {
+    return this.paths.reduce((a, b) => a + b);
+  }
+
+  loadScenario(scenarioName) {
+    this.paths.push(scenarioName.length);
+    console.log(this.paths, this.pathsLengthSum());
+    var scenario = this.scenarios[scenarioName];
+    var scenarioSlide = this.slide(scenario);
+    $('.app').html(scenarioSlide);
+  }
+
+  start() {
+    this.loadScenario('start_scenario');
+
+    $('.app').on('click', 'button', (event) => {
+      var scenario = $(event.target).data('scenario');
+      this.loadScenario(scenario);
+    });
+  }
 }
 
-function loadScenario(scenarioName) {
-  var scenario = scenarios[scenarioName];
-  var scenarioSlide = slide(scenario);
-  $('.app').html(scenarioSlide);
-}
-
-loadScenario('start_scenario');
-
-$('.app').on('click', 'button', event => {
-  var scenario = $(event.target).data('scenario');
-  loadScenario(scenario);
-});
-
-
+var game = new Game(scenarios);
+game.start();
 
 
 
